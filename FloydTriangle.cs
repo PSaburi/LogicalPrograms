df3703f2422cb297a799c0fe72dee538ccd9d1f8
Saburi Parvatkar <saburiparvatkar@gmail.com>
2019-08-30 16:45:29 +0530

    Floyd Triangle

---
+++ FloydTriangle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FloydTriangle
+{
+    class FloydTriangle
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of rows you want in Floyd Triangle");
+            int rows = int.Parse(Console.ReadLine());
+
+            int numberToPrint = 0;
+            for(var i=0; i< rows; i++)
+            {
+                for (var j = 0; j <= i; j++)
+                    Console.Write(++numberToPrint + " ");
+                Console.WriteLine();
+            }
+        }
+    }
+}
